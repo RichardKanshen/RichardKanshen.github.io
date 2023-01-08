f4dfa4af2cfa237b06ca8adb18a581be320d953b
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-01-08 17:54:16 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -2,4 +2,7 @@ window.addEventListener('scroll', function() {
   if (window.scrollY >= 90) {
     document.querySelector('#website-title').style.textAlign = 'left';
   }
+  if (window.scrollY < 90) {
+    document.querySelector('#website-title').style.textAlign = 'center';
+  }
 });
