4acabb7e27d21a5ff475643fbba48eebd9c1d3c2
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-01-09 16:30:17 +0100

    Modified Scroll Detenction

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function() {
   if (window.scrollY >= 90) {
-    document.querySelector('#website-title').classList.add('scroll');
+    document.querySelector('#website-title span').classList.add('scroll');
   }
   if (window.scrollY < 90) {
-    document.querySelector('#website-title').classList.remove('scroll');
+    document.querySelector('#website-title span').classList.remove('scroll');
   }
 });
