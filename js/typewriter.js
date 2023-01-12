eb2290df7f3582e170831b1ccfbf45b128f8dc4f
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-01-12 15:12:43 +0100

    Update typewriter.js

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,25 +1,30 @@
-const texts = ["リチャード", "カンシェン", "RICHARD", "KANSHEN","РИХАРД", "КАНШЕН"];
-const speed = 50;
-const pause = 5000;
+const texts = ["リチャード カンシェン", "RICHARD KANSHEN","РИХАРД КАНШЕН"];
+const speed = 50; //ms
+const pause = 5000; // ms
+const typewriter = document.getElementById("typewriter");
 
 let index = 0;
 let textIndex = 0;
 let currentElement;
 
 function type() {
-    currentElement = document.getElementById(`phrase${Math.ceil(textIndex/2 + 1)}`);
+    currentElement = document.getElementById(`phrase${textIndex + 1}`);
     if (index < texts[textIndex].length) {
-        currentElement.innerHTML += texts[textIndex][index];
+        currentElement.innerHTML += texts[textIndex].charAt(index);
         index++;
         setTimeout(type, speed);
     } else {
-        if(textIndex % 2 == 1){
-            document.getElementById(`phrase${Math.ceil(textIndex/2 + 1)}-br`).innerHTML = "<br>";
-        }
-        index = 0;
-        textIndex++;
-        if (textIndex >= texts.length) textIndex = 0;
-        setTimeout(type, pause);
+        setTimeout(erase, pause);
+ 
     }
 }
+
+function erase() {
+  currentElement.innerHTML = "";
+  index = 0;
+  textIndex++;
+  if (textIndex >= texts.length) textIndex = 0;
+  setTimeout(type, speed);
+}
+
 setTimeout(type, speed);
