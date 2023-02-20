baa41dd24bfc9aec1829beea346b92a8a63fc943
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-02-20 16:01:16 +0100

    Made some quality adjustments
    - Moved itch.summer redir button to the itch.summer section (issue https://github.com/RichardKanshen/RichardKanshen.github.io/issues/5)
    - Fixed itch.summer redir button not working after domain edit (issue https://github.com/RichardKanshen/RichardKanshen.github.io/issues/3)
    - Moved button styles into stylesheet (issue https://github.com/RichardKanshen/RichardKanshen.github.io/issues/4)
    - Removed unnecesary files / unused code (issue https://github.com/RichardKanshen/RichardKanshen.github.io/issues/6)

--- js/loading banner.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-document.querySelector('h1').classList.add('show');
-}, 1000);
--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-if (window.pageYOffset > 90) {
-document.querySelector("h1").classList.add("scroll");
-}
-else {
-document.querySelector("h1").classList.remove("scroll");
-}
-});
